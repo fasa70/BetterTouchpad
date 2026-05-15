0674b773598933427e4c4567a8356f465ba8eb0e
fasa70 <213198939+fasa70@users.noreply.github.com>
2026-05-15 15:25:31 +0800

    更新readme和版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fasa70.bettertouchpad"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.2.0"
+        versionCode = 6
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
